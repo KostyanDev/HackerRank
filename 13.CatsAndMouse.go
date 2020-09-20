e7b52907f590ef27a31011d9efba09ce0b0dd199
Константин Овчаренко <wowbufi@gmail.com>
2020-09-21 00:18:42 +0300

    task for weekend and fri

---
+++ 13.CatsAndMouse.go
@@ -0,0 +1,27 @@
+func abs(n int32) int32 {
+	if n >= 0 {
+		return n
+	} else {
+		return -n
+	}
+}
+
+// Complete the catAndMouse function below.
+func catAndMouse(x int32, y int32, z int32) string {
+
+// Declare and initialize the variables
+	var distanceAtoC int32 = abs(x - z)
+	var distanseBtoC int32 = abs(y - z)
+	var returnValue string
+
+// Do the logic
+	if distanceAtoC < distanseBtoC {
+		returnValue = "Cat A"
+	} else if distanceAtoC > distanseBtoC {
+		returnValue = "Cat B"
+	} else {
+		returnValue = "Mouse C"
+	}
+
+return sOutput
+}
---
+++ 14Task.BonAppetit.go
@@ -0,0 +1,15 @@
+func bonAppetit(bill []int32, k int32, b int32) {
+	var sum,moneyBack int32
+
+	for i := 0; i < len(bill); i++ {
+		sum += bill[i]
+	}
+
+	moneyBack = b - (sum - bill[k]) / 2
+
+	if moneyBack == 0 {
+		fmt.Printf("Bon Appetit")
+	} else {
+		fmt.Printf("%d", moneyBack)
+	}
+}
