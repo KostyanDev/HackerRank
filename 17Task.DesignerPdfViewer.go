e09660aff7e3ba6d686a25fa61a9e20968a71d66
Константин Овчаренко <wowbufi@gmail.com>
2020-09-23 12:05:28 +0300

    Designer Pdf Viewer

---
+++ 17Task.DesignerPdfViewer.go
@@ -0,0 +1,16 @@
+//https://www.hackerrank.com/challenges/designer-pdf-viewer/problem
+func designerPdfViewer(h []int32, word string) int32 {
+
+	var currentHeight, returnValue int32
+	var maxHeight int32 = h[word[0] - 'a']
+
+	for i := 1; i < len(word); i++ {
+		currentHeight = h[word[i] - 'a']
+		fmt.Println(currentHeight)
+		if currentHeight > maxHeight {
+			maxHeight = currentHeight
+		}
+	}
+	returnValue = maxHeight * int32((len(word)))
+	return returnValue
+}
