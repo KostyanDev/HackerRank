59f2c058e668cd4badb94b77f69c8b355df09c08
KostyanDev <>
2020-10-19 10:39:34 +0300

    jumpin on clouds

--- 42Task.JumpingOnTheClouds.go
+++ 42Task.JumpingOnTheClouds.go
@@ -3,29 +3,28 @@ package main
 
 func jumpingOnClouds(c []int32) int32 {
 
-	// Declare and initialize the variables
 	var i int = 0
-	var i32HoopCounter int32 = 0
+	var returnValue int32 = 0
 
 	// Do the loop
 	for i < len(c) {
 		if i + 2 < len(c) {
 			if c[i + 2] == 0 {
 				i += 2
-				i32HoopCounter++
+				returnValue++
 			} else if c[i + 2] == 1 {
 				i += 1
-				i32HoopCounter++
+				returnValue++
 			}
 		} else if i + 1 < len(c) {
 			if c[i + 1] == 0 {
 				i += 1
-				i32HoopCounter++
+				returnValue++
 			}
 		} else if i < len(c) {
 			break
 		}
 	}
 
-	return i32HoopCounter
+	return returnValue
 }
